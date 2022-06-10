9702ef7ea65bb3c2fbe911a81afce0cb79ded522
SergeyKuligin <gosobes@gmail.com>
2022-06-10 19:12:02 +0400

    vic

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './functionBased/App.css';
+import TodoContainer from './functionBased/components/TodoContainer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
